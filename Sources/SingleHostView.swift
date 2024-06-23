fd06afe06dfc36b7ffbeaeb39659dd63d1774b26
S1D1T1 <tiki_acrylic.0f@icloud.com>
2024-06-22 22:28:14 -0400

    fix key overrides

--- Sources/SingleHostView.swift
+++ Sources/SingleHostView.swift
@@ -9,13 +9,13 @@ import SwiftUI
 
 public final class SingleHostingView<Content: View>: NSHostingView<Content> {
 
-  public var onKey:((_ :NSEvent)->Void)?
+  public var onKey:((_ :NSEvent)->Bool)?
 
   required init?(coder: NSCoder) {
     fatalError("init(coder:) has not been implemented")
   }
 
-  init(onKey: ((_ :NSEvent)->Void)? = nil,rootView: Content) {
+  init(onKey: ((_ :NSEvent)->Bool)? = nil,rootView: Content) {
     self.onKey = onKey
     super.init(rootView: rootView)
   }
@@ -25,9 +25,11 @@ public final class SingleHostingView<Content: View>: NSHostingView<Content> {
   }
 
   public override func keyDown(with:NSEvent){
-    if let onKey {
-      onKey(with)
+    if let onKey,
+       onKey(with){
+      return
     }
+    super.keyDown(with: with)
   }
  
 }
